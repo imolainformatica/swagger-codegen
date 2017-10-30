7ea866d5319387d04fb7ed204822612e65e55180
Giacomo Canevale <gcarnevale@imolinfo.it>
2017-10-30 10:53:07 +0100

    Added description, license and info properties

--- samples/server/petstore/java-apache-camel/src/main/java/io/swagger/api/ApiConfiguration.java
+++ samples/server/petstore/java-apache-camel/src/main/java/io/swagger/api/ApiConfiguration.java
@@ -15,9 +15,15 @@ public abstract class ApiConfiguration extends RouteBuilder {
             .contextPath("/v2")
             .bindingMode(RestBindingMode.auto)
             .apiContextPath("/api-doc")
-            .apiProperty("api.title", "")
             .apiProperty("schemes", "http")
-            .apiProperty("api.description", "")
-            .apiProperty("api.version", "1.0.0") : restConfigurationDefinition;
+            .apiProperty("api.title", "")
+            .apiProperty("api.description", "This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key &#x60;special-key&#x60; to test the authorization filters.")
+            .apiProperty("api.version", "1.0.0")
+            .apiProperty("api.termsOfService", "")
+            .apiProperty("api.contact.name", "")
+            .apiProperty("api.contact.email", "apiteam@swagger.io")
+            .apiProperty("api.contact.url", "")
+            .apiProperty("api.license.name", "Apache-2.0")
+            .apiProperty("api.license.url", "http://www.apache.org/licenses/LICENSE-2.0.html") : restConfigurationDefinition;
     }
 }
